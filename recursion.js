86931dd095b186da261636f3932c00500d400766
sudhir <sudhira@gmail.com>
2024-06-09 13:36:44 +0530

    recursion

---
+++ recursion.js
@@ -0,0 +1,22 @@
+// recursion: function called itself again and again is called as recursion
+
+// function recurse(x){
+//    console.log(x);
+//     if(x<5){
+//         recurse(x+1);
+//     }
+
+// }
+// let data=0;
+// recurse(data)
+
+//factorial
+
+function factorial(item) {
+  if (item == 0) {
+    return 1;
+  }
+  return item * factorial(item - 1);
+}
+let data = 5;
+console.log(factorial(data));
